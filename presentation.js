d9f935b5778169a468f784b70cb82631ab4dcf99
Tonio <antoine.doussan@campus-numerique-in-the-alps.com>
2018-06-12 10:38:43 +0200

    Récupération des cordonnées i et j au click

---
+++ presentation.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+
+$('td').click(function() {
+	$(this).attr("i");
+	$(this).attr("j");
+
+	console.log($(this).attr("i"));
+	console.log($(this).attr("j"));
+})
+
+
+
+
+
+
+
+})
--- script.js
+++ script.js
@@ -21,32 +21,7 @@ for (let i=0; i <4; i++) {
 
 
 
-// function createTable(cases) {
-//   var table = document.createElement('table');
-//   var tableBody = document.createElement('tbody');
 
-//   cases.forEach(function(rowData) {
-//     var row = document.createElement('tr');
 
-//     rowData.forEach(function(cellData) {
-//       var cell = document.createElement('td');
-//       cell.appendChild(document.createTextNode(cellData));
-
-//       row.appendChild(cell);
-//     });
-
-//     tableBody.appendChild(row);
-//   });
-
-//   table.appendChild(tableBody);
-//   document.body.appendChild(table);
-// }
-
-// createTable([
-//   [1, 2, 3, 4],
-//   [5, 6, 7, 8],
-//   [9, 10, 11, 12],
-//   [13, 14, 15, 0],
-// ]);
 
 })
